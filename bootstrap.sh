aa8cc46290505d24ee0545647f5fb0490b440f0a
mlepine <p.michael.lepine@gmail.com>
2012-09-03 21:25:24 -0400

    formatting

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,15 +2,15 @@
 cd "$(dirname "$0")"
 git pull
 function doIt() {
-rsync --exclude ".git/" --exclude --exclude "bootstrap.sh" --exclude "README.md" -av . ~
+	rsync --exclude ".git/" --exclude --exclude "bootstrap.sh" --exclude "README.md" -av . ~
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-doIt
+	doIt
 else
-read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-doIt
-fi
+	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+	echo
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		doIt
+	fi
 fi
 unset doIt
